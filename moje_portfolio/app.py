7112c346645ec6cabb2abba83c17bc07f0518368
Samuel <24314@student.vitkovickastredni.cz>
2025-11-05 09:12:19 +0100

    GET - vyhledávání

--- moje_portfolio/app.py
+++ moje_portfolio/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from data import users, nazev_webu, titulek_webu, popis, technologie
 from generator import generate_number
 app = Flask (__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    titulek_webu = ("Portfolio od Marsiny")
-    nazev_webu = ("Samovo portfolio")
-    popis = ("Ukázka projektů, výuka s AI, a testování JItsi2")
-    technologie = ("Flask", "Python", "HTML", "CSS", "Jitsi2", "Copilot", "Javascript", "C#") 
+    email = None
+    if request.method == "GET":
+        email = request.args.get("email")
+        
+    return render_template("index.htm", nazev_webu = nazev_webu)
 
 @app.route("/generator")
 def generator():
