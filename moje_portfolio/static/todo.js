40d5ddb984d1129566c7e23002e8497049f16977
Samuel <24314@student.vitkovickastredni.cz>
2026-01-09 09:40:26 +0100

    09012026

--- moje_portfolio/static/todo.js
+++ moje_portfolio/static/todo.js
@@ -1,13 +1,24 @@
 
-    const input = document.getElementById("inputTask");
-    const createBtn = document.querySelector('form input [type="button"]');
-    const inProcess = document.getElementById("inProcess");
-    const outProcess = document.getElementById("outProcess");
+const input = document.getElementById("inputTask");
+const createBtn = document.querySelector('form input [type="button"]');
+const inProcess = document.getElementById("inProcess");
+const outProcess = document.getElementById("outProcess");
 
-    const span = document.createElement("span");
-    const br = document.createElement("br");
+const span = document.createElement("span");
+const br = document.createElement("br");
 
-    const inputCheckbox = document.createElement ("input");
-    inputCheckbox.type = "checkbox";
+const inputCheckbox = document.createElement ("input");
+inputCheckbox.type = "checkbox";
 
-    createBtn.addEventListener("click", () => {})
+function addTask(text) {
+
+
+    
+}
+
+createBtn.addEventListener("click", () => {
+console.log(input.value);
+input.value = "";
+input.focus();
+
+});
