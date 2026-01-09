f11d54f85b1ad4ab1be839a42d89ce83aa940e0e
Samuel <24314@student.vitkovickastredni.cz>
2026-01-09 09:04:53 +0100

    09012026

---
+++ moje_portfolio/static/todo.js
@@ -0,0 +1,13 @@
+
+    const input = document.getElementById("inputTask");
+    const createBtn = document.querySelector('form input [type="button"]');
+    const inProcess = document.getElementById("inProcess");
+    const outProcess = document.getElementById("outProcess");
+
+    const span = document.createElement("span");
+    const br = document.createElement("br");
+
+    const inputCheckbox = document.createElement ("input");
+    inputCheckbox.type = "checkbox";
+
+    createBtn.addEventListener("click", () => {})
