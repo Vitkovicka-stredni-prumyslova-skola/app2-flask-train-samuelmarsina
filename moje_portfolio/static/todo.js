6489ef08d1d2f06c9e0404de88636123551911a7
Samuel <24314@student.vitkovickastredni.cz>
2026-01-14 09:40:33 +0100

    14012026

--- moje_portfolio/static/todo.js
+++ moje_portfolio/static/todo.js
@@ -17,10 +17,26 @@ function addTask(text) {
     const inputCheckbox = document.createElement ("input");
     inputCheckbox.type = "checkbox";
 
-    inProcess.appendChild{span};
-    inProcess.appendChild{br};
-    inProcess.appendChild{inputCheckbox};
+    inProcess.appendChild(span);
+    inProcess.appendChild(br);
+    inProcess.appendChild(inputCheckbox);
+
+    inputCheckbox.addEventListener("change", () => {
+        const doneCheckBox = document.createElement("input");
+        doneCheckBox.type = "checkbox";
+        doneCheckBox.checked = true;
+
+        const doneSpan = document.createElement("span");
+        doneSpan.textContent = span.textContent;¨
+        doneSpan.classList.add("done");
+
+        const doneBr = document.createElement ("Br");
+
+        outProcess.appendChild(doneCheckbox);
+        outProcess.appendChild(doneSpam);
+        outProcess.appendChild(doneBr);
 }
+)
 
 createBtn.addEventListener("click", () => {
     console.log(input.value);
